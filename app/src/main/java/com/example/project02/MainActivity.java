a030141b4548da719b8962d30fa783a6aa80c98e
Rahim Siddiq <rsiddiq@csumb.edu>
2024-07-26 20:10:57 -0700

    starting navigation setup with onClickListeners and intents

--- app/src/main/java/com/example/project02/MainActivity.java
+++ app/src/main/java/com/example/project02/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project02;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button signUpButton = findViewById(R.id.signUp);
+        signUpButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+            startActivity(intent);
         });
     }
 }
