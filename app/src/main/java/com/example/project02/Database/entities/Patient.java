4661b2bdd205c786b1cf602959f37f9a2d830c24
garykuepper <gkuepper9@gmail.com>
2024-07-29 19:40:25 -0700

    Add constructor for patient and getters and setters

--- app/src/main/java/com/example/project02/Database/entities/Patient.java
+++ app/src/main/java/com/example/project02/Database/entities/Patient.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.Objects;
+
 @Entity
 public class Patient {
 
@@ -19,4 +21,96 @@ public class Patient {
     private String state;
     private String zipCode;
     private String birthDate;  // maybe use date object?
+
+    public Patient(String birthDate, String firstName, String lastName, String ssn) {
+        this.birthDate = birthDate;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Patient patient = (Patient) o;
+        return id == patient.id && Objects.equals(ssn, patient.ssn) && Objects.equals(lastName, patient.lastName) && Objects.equals(firstName, patient.firstName) && Objects.equals(street, patient.street) && Objects.equals(city, patient.city) && Objects.equals(state, patient.state) && Objects.equals(zipCode, patient.zipCode) && Objects.equals(birthDate, patient.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ssn, lastName, firstName, street, city, state, zipCode, birthDate);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
 }
