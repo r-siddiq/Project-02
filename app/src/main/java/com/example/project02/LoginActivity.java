1f2386ec34372ec89ffe21d1b2eb7b4e932ad6bf
jmhammond08 <jess.hammond90@gmail.com>
2024-08-05 23:02:27 -0700

    Added buildFeatures { viewBinding true } to gradle. Created LoginActivity.java and will migrate main_activity to login_activity

---
+++ app/src/main/java/com/example/project02/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.project02;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project02.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
+                startActivity(intent);
+            }
+        });
+    }
+
+    static Intent loginIntentFactory(Context context){
+        return new Intent(context, LoginActivity.class);
+    }
+}
--- app/src/main/java/com/example/project02/MainActivity.java
+++ app/src/main/java/com/example/project02/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.project02;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,14 +11,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.project02.Database.AppRepository;
+import com.example.project02.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.project02.MAIN_ACTIVITY_USER_ID";
+
+    private ActivityMainBinding binding;
+
+    private AppRepository repository;
+
     public static final String TAG = "GRP7_Pill_Hub";
+
+    int loggedInPatientID = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loginUser();
+
+        if(loggedInPatientID == -1){
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+
+/*        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -35,6 +57,17 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
             startActivity(intent);
-        });
+        });*/
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int PatientID){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, PatientID);
+        return intent;
+    }
+
+    private void loginUser() {
+        //TODO: create login method.
+        loggedInPatientID = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
     }
 }
