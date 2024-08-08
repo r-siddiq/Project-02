32a8b1abbc08df7e6cb8f55da9ea469b4a831dbe
Rahim Siddiq <rsiddiq@csumb.edu>
2024-08-08 02:00:56 -0700

    .

--- app/src/main/java/com/example/project02/Database/DrugDAO.java
+++ app/src/main/java/com/example/project02/Database/DrugDAO.java
@@ -1,7 +1,32 @@
 package com.example.project02.Database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.project02.Database.entities.Drug;
 
 @Dao
 public interface DrugDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Drug... drug);
+
+    @Delete
+    void delete(Drug drug);
+
+    @Query("SELECT * FROM " + PharmacyDatabase.DRUG_TABLE + " ORDER BY name")
+    LiveData<Drug> getAllDrugs();
+
+    @Query("DELETE FROM " + PharmacyDatabase.DRUG_TABLE)
+    void deleteAll();
+
+    @Query("SELECT * FROM " + PharmacyDatabase.DRUG_TABLE + " WHERE name == :drugName")
+    LiveData<Drug> getDrugByName(String drugName);
+
+    @Query("SELECT * FROM " + PharmacyDatabase.DRUG_TABLE + " WHERE id == :id")
+    LiveData<Drug> getUserByUserId(int id);
 }
--- app/src/main/java/com/example/project02/Database/PatientDAO.java
+++ app/src/main/java/com/example/project02/Database/PatientDAO.java
@@ -1,11 +1,7 @@
 package com.example.project02.Database;
 
 import androidx.lifecycle.LiveData;
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
+import androidx.room.*;
 
 import com.example.project02.Database.entities.Patient;
 
@@ -20,18 +16,18 @@ public interface PatientDAO {
     @Delete
     void delete(Patient user);
 
-    @Query("SELECT * from " + AppDatabase.PATIENT_TABLE + " ORDER BY username")
+    @Query("SELECT * from " + PharmacyDatabase.PATIENT_TABLE + " ORDER BY username")
     LiveData<List<Patient>> getAllUsers();
 
-    @Query("DELETE from " + AppDatabase.PATIENT_TABLE)
+    @Query("DELETE from " + PharmacyDatabase.PATIENT_TABLE)
     void deleteAll();
 
-    @Query("SELECT * from " + AppDatabase.PATIENT_TABLE + " WHERE username == :username" )
+    @Query("SELECT * from " + PharmacyDatabase.PATIENT_TABLE + " WHERE username == :username" )
     LiveData<Patient> getPatientByUsername(String username);
 
-    @Query("SELECT * from " + AppDatabase.PATIENT_TABLE + " WHERE id == :userId" )
+    @Query("SELECT * from " + PharmacyDatabase.PATIENT_TABLE + " WHERE id == :userId" )
     LiveData<Patient> getPatientByUserId(int userId);
 
-    @Query("SELECT COUNT(*) FROM " + AppDatabase.PATIENT_TABLE + " WHERE username = :username")
+    @Query("SELECT COUNT(*) FROM " + PharmacyDatabase.PATIENT_TABLE + " WHERE username = :username")
     LiveData<Integer> countUsersByUsername(String username);
 }
