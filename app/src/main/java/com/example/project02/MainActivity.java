ccde0f0997c621ad6a22348847019e9f5adceda6
jmhammond08 <jess.hammond90@gmail.com>
2024-08-08 13:27:16 -0700

    User delete functioning

--- app/src/main/java/com/example/project02/MainActivity.java
+++ app/src/main/java/com/example/project02/MainActivity.java
@@ -54,6 +54,13 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
+        binding.deleteUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteUser();
+            }
+        });
+
 
         //updateDisplay();
     }
