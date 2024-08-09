e4c087eac2d1cb936606cd2bf78890bdebc06912
jmhammond08 <jess.hammond90@gmail.com>
2024-08-08 17:17:39 -0700

    Created PrescriptionEntryActivity.java

--- app/src/main/java/com/example/project02/PrescriptionEntryActivity.java
+++ app/src/main/java/com/example/project02/PrescriptionEntryActivity.java
@@ -1,18 +1,11 @@
 package com.example.project02;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.project02.R;
-
-public class RegistrationActivity extends AppCompatActivity {
+public class PrescriptionEntryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
